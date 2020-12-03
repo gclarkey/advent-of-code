8cd21a44934f73e0d32f4aa8e6ab1fa6a47a755e
gareth_surecert <gareth.clarke@surecert.com>
2020-12-03 17:28:51 +0000

    day3

--- src/main/java/ie/gc/aoc/Aoc.java
+++ src/main/java/ie/gc/aoc/Aoc.java
@@ -2,6 +2,7 @@ package ie.gc.aoc;
 
 import ie.gc.aoc.day1.Day1;
 import ie.gc.aoc.day2.Day2;
+import ie.gc.aoc.day3.Day3;
 import ie.gc.aoc.input.FileInput;
 import ie.gc.aoc.input.Input;
 
@@ -19,6 +20,10 @@ public class Aoc {
         System.out.println("\n\n**** DAY 2 ****");
         final Day2 day2 = new Day2(input);
         day2.doSomething();
+
+        System.out.println("\n\n**** DAY 3 ****");
+        final Day3 day3 = new Day3(input);
+        day3.doSomething();
     }
 
 }
---
+++ src/main/java/ie/gc/aoc/day3/Day3.java
@@ -0,0 +1,55 @@
+package ie.gc.aoc.day3;
+
+import ie.gc.aoc.input.Input;
+
+import java.util.List;
+
+public class Day3 {
+
+    private final Input input;
+
+    public Day3(Input input) {
+        this.input = input;
+    }
+
+    public void doSomething() {
+        final List<String> stringInput = input.fetchInput("/day3/map.txt");
+        System.out.println("stringInput.size()="+stringInput.size());
+
+        final int treesEncountered_3_1 = numberOfTreesEncountered(stringInput, 3, 1);
+        System.out.println("Number of r3d1 trees encountered: " + treesEncountered_3_1);
+
+        final int treesEncountered_1_1 = numberOfTreesEncountered(stringInput, 1, 1);
+        final int treesEncountered_5_1 = numberOfTreesEncountered(stringInput, 5, 1);
+        final int treesEncountered_7_1 = numberOfTreesEncountered(stringInput, 7, 1);
+        final int treesEncountered_1_2 = numberOfTreesEncountered(stringInput, 1, 2);
+
+        System.out.println("Number of all trees encountered: " + treesEncountered_3_1*treesEncountered_1_1*treesEncountered_5_1*treesEncountered_7_1*treesEncountered_1_2);
+    }
+
+    private int numberOfTreesEncountered(final List<String> stringInput, int right, int down) {
+        int x=0;
+        int y=0;
+        int treesEncountered = 0;
+        for(int i = 0; i< stringInput.size()-1; i++){
+            x +=right;
+            if(x >= stringInput.get(y).length()){
+                x = x - stringInput.get(y).length();
+            }
+
+            y=y+down;
+            if(y>= stringInput.size()){
+                break;
+            }
+
+            if(stringInput.get(y).charAt(x) == '#'){
+                treesEncountered++;
+            }
+        }
+
+        System.out.println("Number of trees encountered: " + treesEncountered);
+
+        return treesEncountered;
+    }
+
+}
